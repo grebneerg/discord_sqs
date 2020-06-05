1050c60fc49e2f55f4749e44b57a23bb07c04db5
Jack Greenberg <kcaj@grebneerg.com>
2020-06-05 19:09:29 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const AWS = require('aws-sdk');
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+AWS.config.update({region:'us-east-1'});
+const sqs = new AWS.SQS();
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+	sqs.sendMessage({
+		MessageBody: JSON.stringify(msg),
+		QueueUrl: process.argv[3],
+	}).promise()
+});
+
+client.login(process.argv[2]);
